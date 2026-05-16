dad58b1ce075fbaea8592ad151cff4848e775936
Subhajit Das <dassubhojit750075@gmail.com>
2026-05-16 17:06:28 +0530

    Fix production API environment variable

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const baseURL =
-  process.env
-    .NEXT_PUBLIC_API_BASE_URL ??
-  "http://127.0.0.1:8000/api";
+  process.env.NEXT_PUBLIC_API_URL ??
+  "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL,
